06f5003e59c7e660c3da4c1f2f96b39590b286e9
Aleksandar Dimitrov <netbull2007@gmail.com>
2017-02-28 21:49:57 +0200

    psr-1 compability

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,8 +18,6 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-//        $rootNode = $treeBuilder->root('netbull_auth');
-
 
         return $treeBuilder;
     }
